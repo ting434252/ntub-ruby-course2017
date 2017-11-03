b42e3fae84844128cc8c051a081c5e53e2603f1b
謝佳瑋 <x5758x@gmail.com>
2017-11-03 11:12:44 +0800

    TEST 訂單功能

--- 10446005/order.rb
+++ 10446005/order.rb
@@ -1,16 +1,30 @@
 class Order
 
+
+  attr_reader :items
   attr_reader :state
 
-  def initialize
+  def initialize(products=[])
 
     @state = "pending"
+    if !products.empty?
+      @items = products
+    end
   end
 
   def pay!
 
     @state = "paid"
     "paid"
+  end
+
+  def total_price
+
+    if !@items.empty?
+      @items.map { |item|  item[:quantity]*item[:price] }.inject(:+)
+    else
+      0
+    end
   end
 
 end
