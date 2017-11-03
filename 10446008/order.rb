93f9504fd6c0aa181bfc05af58729642a29f9a2b
Drunces <sq851023@gmail.com>
2017-11-03 11:27:44 +0800

    add order computing

--- 10446008/order.rb
+++ 10446008/order.rb
@@ -10,4 +10,17 @@ class Order
     @state = "paid"
   end
 
+  def items
+    @products
+  end
+
+  def total_price
+    amount = 0
+    for product in @products
+      price, quantity = product[:price], product[:quantity]
+      amount += price * quantity if quantity >= 0 and price >= 0
+    end
+    amount
+  end
+
 end
