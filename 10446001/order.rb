f0b2446939ab9eb7a63eeb6fd6d6e99385f3f9f8
李玉棋 <32380815+10446001@users.noreply.github.com>
2017-11-04 18:24:02 +0800

    computing total_price

--- 10446001/order.rb
+++ 10446001/order.rb
@@ -1,12 +1,26 @@
 class Order
   attr_reader :state
+  attr_reader :products         #可讀
 
-  def initialize
+  def initialize(products=[])
   @state = "pending"  #狀態為未處理
+  @products = products
   end
   
   def pay!
     @state = "paid"     #狀態為付款
+  end
+  
+  def items
+    @products           #商品筆數
+  end
+
+  def total_price
+    total = 0
+    for product in @products            #products陣列
+      total += product[:quantity] * product[:price]         #數量*價格
+    end
+    total
   end
 
 end
