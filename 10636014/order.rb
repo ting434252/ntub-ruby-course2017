f242bc79d85e9a97b8e1a4d966c3b252d229b8c9
jing1981jing1981 <jing1981jing1981@gmail.com>
2017-11-04 06:30:12 -0400

    add total_price Method

--- 10636014/order.rb
+++ 10636014/order.rb
@@ -1,12 +1,18 @@
 class Order
-  attr_reader :state
+  attr_reader :state , :items 
   
-  def initialize
+  def initialize (product=[])
     @state = "pending"
+    @items=product  
   end
   
   def pay!
     @state = "paid"
   end  
+  
+  def total_price
+    items.reduce(0) { |sum, item| sum + (item[:quantity]*item[:price]) }
+  end 
 
 end
+
