68029ee1dfd639786a2ea97e8e76ae3cbfdc5d8f
Andre Wu <secret8419@gmail.com>
2017-11-06 17:47:58 +0800

    add method of total_price

--- 10446006/order.rb
+++ 10446006/order.rb
@@ -1,11 +1,24 @@
 class Order
-  attr_reader :state
+  attr_reader :state , :buy_list
 
-  def initialize
+  def initialize(buy_list = [])
     @state = "pending"
+    @buy_list = buy_list
   end
 
   def pay!
     @state = "paid"
+  end
+
+  def items
+    @buy_list
+  end
+
+  def total_price
+    sumprice = 0
+
+    @buy_list.reduce(0) {
+      |sumprice, buy_list| sumprice + buy_list[:price] * buy_list[:quantity]
+    }
   end
 end
