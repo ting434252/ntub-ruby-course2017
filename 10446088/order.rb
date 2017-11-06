5dae3a270672b217f4c7a66e1579515128dedcb7
Jui-chuan Hsu <jchsu@ntub.edu.tw>
2017-11-06 10:22:27 +0800

    homework004 2nd 計算訂單金額

--- 10446088/order.rb
+++ 10446088/order.rb
@@ -1,7 +1,9 @@
 class Order
+  attr_reader :status, :goods
 
-  def initialize
+  def initialize(goods = [])
     @status = "pending"
+    @goods = goods
   end
 
   def state
@@ -10,6 +12,14 @@ class Order
 
   def pay!
     @status = "paid"
+  end
+
+  def items
+    @goods
+  end
+
+  def total_price
+    goods.reduce(0) { |total, i| total + i[:quantity] * i[:price] }
   end
 
 end
