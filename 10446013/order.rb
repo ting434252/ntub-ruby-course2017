894db1b47083a84cf2e9bf58e042f0a2d64aefaf
mushroomchen0429 <32380829+mushroomchen0429@users.noreply.github.com>
2017-11-06 19:54:27 +0800

    add items & total_price

--- 10446013/order.rb
+++ 10446013/order.rb
@@ -1,11 +1,21 @@
 class Order
   attr_reader :state
   
-  def initialize
+  def initialize(productsArray=[])
     @state = "pending"
+    @products = productsArray
   end
   
   def pay!
     @state = "paid"
+  end
+  
+  def items
+    @products
+  end
+  
+  def total_price
+    total = 0
+    @products.reduce(0) {  |total, products| total += products[:quantity] * products[:price] }
   end
 end
