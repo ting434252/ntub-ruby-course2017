55347149e471dfceeed1b9a8d0d2dd5166cf0df2
Jui-chuan Hsu <jchsu@ntub.edu.tw>
2017-11-06 10:19:20 +0800

    homework004 2nd pending

--- 10446088/order.rb
+++ 10446088/order.rb
@@ -1,28 +1,12 @@
 class Order
+  attr_reader :status
 
-  def initialize(goods = [])
+  def initialize
     @status = "pending"
-    @goods = goods
   end
 
   def state
     @status
   end
 
-  def pay!
-    @status = "paid"
-  end
-
-  def items
-    @goods
-  end
-
-  def total_price
-    total = 0
-    @goods.each do |good|
-      total += good[:quantity] * good[:price]
-    end
-    total
-  end
-    
 end
