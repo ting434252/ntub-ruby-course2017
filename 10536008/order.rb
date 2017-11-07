d889143c55c18b869d36e2280ccdb12c466d14fe
yingting <32380760+ting434252@users.noreply.github.com>
2017-11-07 20:35:01 +0800

    add items and price

--- 10536008/order.rb
+++ 10536008/order.rb
@@ -1,10 +1,24 @@
 class Order
   attr_reader :state, :products
-  def initialize
+  
+  def initialize (products = [])
     @state = "pending"
+    @products = products
   end
   
   def pay!
     @state = "paid"
+  end
+  
+  def items 
+    @items = products
+  end
+  
+  def total_price
+    total = 0
+    for product in products
+      total += product[:quantity] * product[:price]
+    end
+    total
   end
 end
