91fd584f8abd531b91d51452e513e9b41f232ed4
Luck626kkk <32380828+Luck626kkk@users.noreply.github.com>
2017-11-09 15:15:12 +0800

    update reduce

--- 10536016/order.rb
+++ 10536016/order.rb
@@ -16,10 +16,11 @@ class Order
 
   def total_price
       total = 0
-   for product in products
-     total += product[:price]*product[:quantity]
-   end
-    total
+#    for product in products
+#      total += product[:price]*product[:quantity]
+#    end
+#     total
+      products.reduce(0) {|total, product| total + product[:price] * product[:quantity] }
   end
 end
     
