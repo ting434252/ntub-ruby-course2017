da2b8b9c04b057069db73d16ea08ba4d094fcb2d
IffyArt <iffyart1234@gmail.com>
2017-11-09 20:08:14 +0800

    刪除根目錄剩餘的DS_Store與修改為reduce方法

--- 10446007/order.rb
+++ 10446007/order.rb
@@ -1,29 +1,24 @@
 class Order
-    attr_accessor :state
+  attr_accessor :state
+  attr_reader :products
 
-    def initialize(products=[])
-        @state = "pending"
-        @products = products
-    end
+  def initialize(products=[])
+    @state = "pending"
+    @products = products
+  end
 
-    def pay!
-        @state = "paid"
-    end
+  def pay!
+    @state = "paid"
+  end
 
-    def items
-        @products
-    end
+  def items
+    @products
+  end
 
-    def total_price
-        amount = 0
-        for product in @products
-            price, quantity = product[:price], product[:quantity]
-            if price>0 && quantity>0
-            amount += price * quantity 
-            end
-        end
-        amount
-    end
+  def total_price
+    amount = 0
+    @products.reduce(0) {|amount, products| amount + products[:price] * products[:quantity]}
+  end
 end
 
 
